1ec2429aadcc2bae9d9b894b3f2576b81ca98b27
Soham De <sohamde2004@gmail.com>
2025-11-10 23:29:56 +0530

    OTP entity

--- auth-service/src/main/java/com/shutterlink/auth_service/services/AuthService.java
+++ auth-service/src/main/java/com/shutterlink/auth_service/services/AuthService.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 public class AuthService {
@@ -70,22 +68,22 @@ public class AuthService {
         Auth user = authRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Invalid token"));
 
-        return new TokenValidationResponseDTO(true, user.getAuthId().toString(), user.getUsername(), user.getRole().name());
+        return new TokenValidationResponseDTO(true, user.getId().toString(), user.getUsername(), user.getRole().name());
    
     }
 
    public void changePassword(ChangePasswordDTO req) {
-        // You’d typically fetch the user from SecurityContext
-        // For now, assume current user’s email is known
-        String currentEmail = "user@example.com";
-        Auth user = authRepository.findByEmail(currentEmail)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        // // You’d typically fetch the user from SecurityContext
+        // // For now, assume current user’s email is known
+        // String currentEmail = "user@example.com";
+        // Auth user = authRepository.findByEmail(currentEmail)
+        //         .orElseThrow(() -> new RuntimeException("User not found"));
 
-        if (!BCrypt.checkpw(req.getOldPassword(), user.getPassword()))
-            throw new RuntimeException("Incorrect old password");
+        // if (!BCrypt.checkpw(req.getOldPassword(), user.getPassword()))
+        //     throw new RuntimeException("Incorrect old password");
 
-        user.setPassword(BCrypt.hashpw(req.getNewPassword(), BCrypt.gensalt()));
-        authRepository.save(user);
+        // user.setPassword(BCrypt.hashpw(req.getNewPassword(), BCrypt.gensalt()));
+        // authRepository.save(user);
     }
 
 }
