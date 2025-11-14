39f613de6a5ca9e1b129f68e91b58364922f1024
ayushman1626 <ayushmanpal1626@gmail.com>
2025-11-15 01:00:04 +0530

    added eureka-server

---
+++ auth-service/src/test/java/com/shutterlink/auth_service/services/AuthServiceTests.java
@@ -0,0 +1,19 @@
+package com.shutterlink.auth_service.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthServiceTests {
+    
+
+    @Test
+    public void contextLoads() {
+        
+        assertEquals(4, 2+2);
+
+    }
+
+}
